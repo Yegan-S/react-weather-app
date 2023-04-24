8e671b8c2c9966e22fb3e8e19bd0fd5185667c9a
sedi <salehisediqe86@gmail.com>
2023-04-24 01:57:50 -0700

    Added the GitHub repository

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather app
+     <h1> Weather app
+      </h1>
+      <footer>
+        This project was coded by Sedii and is{" "}
+      <a href="https://github.com/sedi-yegan/react-weather-app" target="_blank" rel="noreferrer">
+ open-sourced on Github.
+      </a>
+      </footer>
     </div>
   );
 }
