969992147c5f988a095d79a81478aa7d6329ce57
sedi <salehisediqe86@gmail.com>
2023-04-25 03:08:03 -0700

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -8,27 +8,33 @@ export default function Weather(){
             <form>
                 <div className="row">
                     <div className="col-9">
-                <input type="search" placeholder="Enter a city..." className="form-control" />
+                <input type="search" placeholder="Enter a city..." className="form-control" outFocus="on" />
             </div>
             <div className="col-3">
             <input type="submit" value="search" className="btn btn-primary"  />
             </div>
             </div>
             </form>
+
            <h1>New York</h1> 
            <ul>
             <li>Wednesday 07:00</li>
             <li>Mostly Cloudy</li>
            </ul>
-           <div className="row">
+
+           <div className="row mt-1">
             <div className="col-6">
-<img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
-alt="partly cloudy"   />
-6°C
-            </div>
-            <div className="col-6">
-<ul>
-    <li>
+       <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+         alt="partly cloudy"   />
+
+        <span className="temperature">6</span> 
+        <span className="unit">°C</span>
+        </div>
+
+
+        <div className="col-6">
+        <ul>
+        <li>
         Precipitation: 15%
     </li>
     <li>
@@ -37,7 +43,7 @@ alt="partly cloudy"   />
     <li>
         Wind: 13 km/h
     </li>
-</ul>
+    </ul>
             </div>
            </div>
             </div>
