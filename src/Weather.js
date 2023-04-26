70037ab8fce18b258df238ce6c485383dcee5a04
sedi <salehisediqe86@gmail.com>
2023-04-26 10:55:05 -0700

    Used the correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ const [city, setCity] = useState(props.defaultCity);
             city:response.data.name,
             date:new Date(response.data.dt * 1000),
             description:response.data.weather[0].description,
-            iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" ,
+            iconUrl:`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
             humidity:response.data.main.humidity,
         });
     }
