fb5ba582b6696c5db4042d3b8f5c7943250da616
sedi <salehisediqe86@gmail.com>
2023-04-29 03:08:56 -0700

    used Effect to update forecast based on city

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect } from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
@@ -8,6 +8,10 @@ export default function WeatherForecast(props){
 
     let[loaded, setLoaded] = useState(false);
      let[forecast, setForecast] = useState(null);
+
+     useEffect(() => {
+setLoaded(false);
+     }, [props.coordinates]);
 
     function handleResponse(response){
 setForecast(response.data.daily);
@@ -19,14 +23,16 @@ if (loaded){
     return (
 <div className="weatherForecast">
 <div className="row">
+    
     {forecast.map(function (dailyForecast, index){
-        if (index < 6){
+
+        if (index < 6) {
         return(
             <div className="col" key={index}>
     <WeatherForecastDay data={dailyForecast} />
 </div>
         );
-         }
+        }
     })}
 
 </div>
